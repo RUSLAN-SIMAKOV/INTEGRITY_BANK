0b2d1047199763e61d09da023ab3bef7ce675212
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2020-03-17 13:38:27 +0200

    initiated spring boot application

---
+++ src/main/java/ruslan/simakov/integritybank/IntegritybankApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.integritybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegritybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegritybankApplication.class, args);
+    }
+
+}
