f5b767f9978511a2e73cd65c6c891d7bae668537
RUSLAN-SIMAKOV <57362068+RUSLAN-SIMAKOV@users.noreply.github.com>
2020-03-19 02:07:19 +0200

    created filtration, sorting, pagination (#3)

--- src/main/java/ruslan/simakov/integritybank/service/TransactionService.java
+++ src/main/java/ruslan/simakov/integritybank/service/TransactionService.java
@@ -3,11 +3,12 @@ package ruslan.simakov.integritybank.service;
 import ruslan.simakov.integritybank.model.Transaction;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface TransactionService {
 
-    List<Transaction> getAllTransactions(Pageable pageable);
+    Page<Transaction> getAllTransactions(Pageable pageable);
 
     Transaction addTransaction(Long transferMoneyFromAccount, Long transferMoneyToAccount,
                                Double amountOfMoneyTransferred);
--- src/main/java/ruslan/simakov/integritybank/service/impl/TransactionServiceImpl.java
+++ src/main/java/ruslan/simakov/integritybank/service/impl/TransactionServiceImpl.java
@@ -6,6 +6,7 @@ import ruslan.simakov.integritybank.service.TransactionService;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,8 @@ public class TransactionServiceImpl implements TransactionService {
   private TransactionRepository transactionRepository;
 
     @Override
-    public List<Transaction> getAllTransactions(Pageable pageable) {
-        return transactionRepository.findAll();
+    public Page<Transaction> getAllTransactions(Pageable pageable) {
+        return transactionRepository.findAll(pageable);
     }
 
   @Override
